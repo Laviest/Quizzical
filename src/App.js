1274d9b8becb3a3ca93d01e940e28c55ace9bb89
Laviest <60656865+Laviest@users.noreply.github.com>
2023-01-23 14:31:18 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Start from './components/Start'
 import {nanoid} from 'nanoid'
 
 export default function App() {
-
-    // fix question to show on other screen
-
     const [allData, setAllData] = React.useState([])
     const [quiz, setQuiz] = React.useState(false)
+    const map = new Map([['num', 0]]);
+    const [correctAnswersArray, setCorrectAnswersArray] = React.useState({5: false, 10: false, 15: false, 25: false, 30: false})
 
     React.useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=10')
@@ -16,33 +15,52 @@ export default function App() {
             .then(data => setAllData(data.results))
     }, [])
 
-    console.log(allData)
     let allQuestions;
 
     const questions = allData.slice(0, 5).map(question => {
         allQuestions = question.incorrect_answers
         allQuestions.push(question.correct_answer)
+        if(allQuestions.length >= 5) {
+            allQuestions.pop()
+        }
+        if(allQuestions.length === 3) {
+            allQuestions.pop()
+        }
         for(let i = 0; i < allQuestions.length; i++) {
             let tempCard = allQuestions[i];
             var randomIndex = Math.floor(Math.random() * allQuestions.length);
             allQuestions[i] = allQuestions[randomIndex];
             allQuestions[randomIndex] = tempCard;
         }
+        map.set('num', map.get('num') + 5 || 5);
         return <Question
-        key={nanoid()}
-        quest={question.question.replace('&quot;', "'").replace('&#039;', '')} 
-        allchoices={allQuestions}
-        choices={question.incorrect_answers}
-        correct={question.correct_answer}
+            key={nanoid()}
+            quest={question.question.replace('&quot;', "'").replace('&#039;', "'")} 
+            allchoices={allQuestions}
+            startNumber={map.get('num')}
+            correct_answer={question.correct_answer}
+            check_answers={() => checkAnswers}
+            answers_array = {correctAnswersArray}
+            answerIsCorrect = {false}
         />})
 
-    
+    React.useEffect(() => {
+        // console.log(questions)
+
+        // console.log(answers[0].props.children[0].props)
+    }, [])
+
+    const checkAnswers = () => {
+        questions.map(question => {
+            console.log(question)
+        })
+    }
 
     return (
         <div>
             {!quiz && <Start startQuiz={() => setQuiz(true)}/>}
             {quiz && questions}
-
+            {quiz && <button onClick={checkAnswers} className='check-answers-btn'>Check answers</button>}
         </div>
     )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,67 @@
 import React from 'react'
 
 export default function Question(props) {
-    const answers = props.allchoices.map(answ => <button className='quest--choice-button'>{answ}</button>)
+    const map1 = new Map([['num', props.startNumber]]);
+    const array = {5: false, 10: false, 15: false, 25: false, 30: false}
+    const btnGroup = props.startNumber;
+    const checkedAnswersMap = new Map([])
+    const correct_answer = props.correct_answer
+
+    const handleRadioButton = (answer) => {
+        // if(answer === correct_answer) {
+        //     checkedAnswersMap.set(btnGroup, true)
+        // } else {
+        //     checkedAnswersMap.set(btnGroup, false)
+        // }
+        if(answer === correct_answer) {
+            props.answerIsCorrect = true
+            array[Number(btnGroup)] = true
+            // checkedAnswersMap.set(btnGroup, true)
+        } else {
+            props.answerIsCorrect = false
+            array[Number(btnGroup)] = false
+            // checkedAnswersMap.set(btnGroup, false)
+        }
+        // console.log(array)
+    }
+
+    function checkAnswers() {
+        // console.log(checkedAnswersMap)
+    }
+
+    const answers = props.allchoices.map(
+        answ => {
+            map1.set('num', map1.get('num') + 1 || 1);
+            return <div className='quest--one'>
+                <input
+                    type="radio"
+                    className='quest--radio-custom'
+                    name={btnGroup.toString()}
+                    value={map1.get('num').toString()}
+                    id={map1.get('num').toString()}
+                    onChange={() => handleRadioButton(answ)}
+                />
+                <label htmlFor={map1.get('num').toString()}>{answ}</label>
+            </div>
+        }
+    )
+
+    // React.useEffect(() => {
+    //     console.log(answers[0].props.children[0].props)
+    // }, answers)
+
+    
     return(
         <div className='quest--div'>
             <div className='quest--blob-one'></div>
             <div className='quest--blob-two'></div>
             <h1 className='quest--title'>{props.quest}</h1>
+            <div className='quest--answers-div'>
             {answers}
+            </div>
+            <div className='hr'>
             <hr></hr>
+            </div>
         </div>
     )
 }
