dec7fe80b33a79cab19bdb8de4080388960c9216
Petar Joshevski <60656865+Laviest@users.noreply.github.com>
2023-05-21 11:45:47 +0200

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,48 +8,17 @@ export default function Question(props) {
     const correct_answer = props.correct_answer
 
     const handleRadioButton = (answer) => {
-        // if(answer === correct_answer) {
-        //     checkedAnswersMap.set(btnGroup, true)
-        // } else {
-        //     checkedAnswersMap.set(btnGroup, false)
-        // }
         if(answer === correct_answer) {
             props.answerIsCorrect = true
             array[Number(btnGroup)] = true
-            // checkedAnswersMap.set(btnGroup, true)
         } else {
             props.answerIsCorrect = false
             array[Number(btnGroup)] = false
-            // checkedAnswersMap.set(btnGroup, false)
         }
-        // console.log(array)
     }
 
     function checkAnswers() {
-        // console.log(checkedAnswersMap)
     }
-
-    // const answers = props.allchoices.map(
-    //     answ => {
-    //         map1.set('num', map1.get('num') + 1 || 1);
-    //         return <div className='quest--one'>
-    //             <input
-    //                 type="radio"
-    //                 className='quest--radio-custom'
-    //                 name={btnGroup.toString()}
-    //                 value={map1.get('num').toString()}
-    //                 id={map1.get('num').toString()}
-    //                 onChange={() => handleRadioButton(answ)}
-    //             />
-    //             <label htmlFor={map1.get('num').toString()}>{answ}</label>
-    //         </div>
-    //     }
-    // )
-
-    // React.useEffect(() => {
-    //     console.log(answers[0].props.children[0].props)
-    // }, answers)
-
     
     return(
         <div className='quest--div'>
